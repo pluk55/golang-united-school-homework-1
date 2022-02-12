888cb76e9efcc65c34437d6fbe613430fe45045f
Konstantin Savelev <konstantin.savelyev@biocatch.com>
2022-02-12 13:39:28 +0200

    init

--- solution.go
+++ solution.go
@@ -1,5 +1,17 @@
-package solution
+package main
+
+import (
+	"fmt"
+	"github.com/kyokomi/emoji/v2"
+)
 
 func GetMessage() string {
-	return ""
+	fmt.Println("Hello World Emoji!")
+
+	emoji.Sprint(":beer: Beer!!!")
+	return emoji.Sprint("Hello :beer:")
+}
+
+func main() {
+	fmt.Println(GetMessage())
 }
