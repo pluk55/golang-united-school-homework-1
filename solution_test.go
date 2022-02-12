35b420a3d52492e6eac41d5eda8fb50c668c59a0
pluk55 <9225534@gmail.com>
2022-02-12 14:05:43 +0200

    added go summ

---
+++ solution_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+	msg := GetMessage()
+
+	if strings.Contains(msg, "Hello") {
+		fmt.Println("Passed")
+
+	}
+}
